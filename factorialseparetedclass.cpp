f8a07dd23ffec1f26b74d362c48f22f90894cf02
MADHURIMA BAG_MCA_2024-26_26_UEMK <madhurimabag4@gmail.com>
2025-02-10 22:39:41 +0530

    10/02/25

---
+++ factorialseparetedclass.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Function to calculate factorial
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int number = 5;
+    int result = factorial(number);
+    printf("Factorial of %d is: %d\n", number, result);
+    return 0;
+}
+
