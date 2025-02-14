849d7d1255a84a9afcb56ba6ae8d854c64edf254
MADHURIMA BAG_MCA_2024-26_26_UEMK <madhurimabag4@gmail.com>
2025-02-14 21:16:00 +0530

    14/02/25

---
+++ swaptwonumber.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Swapping the numbers
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
+
