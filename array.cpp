212810232691d3ef9b607d13dcb5832a162f4272
MADHURIMA BAG_MCA_A_2024-26_26_UEMK <madhurimabag4@gmail.com>
2025-08-16 20:16:24 +0530

    16/08/25

--- array.cpp
+++ array.cpp
@@ -1,7 +1,7 @@
        #include <stdio.h>
 
 int main() {
-    // Create an array of integers
+    // Create an array of integers 
     int numbers[] = {10, 20, 30, 40, 50};
 
     // Access array elements
